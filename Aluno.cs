342c808f9410a5e52c5b32974306763ee65f68bf
Igor Rodrigues <48895884+igorrodriguesss@users.noreply.github.com>
2022-03-16 21:18:03 -0300

    Add files via upload

---
+++ Aluno.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    class Aluno
+    {
+        private string nome;
+        private int idade;
+        private string matricula;
+        private double p1, p2;
+
+        public double CalcularMedia()
+        {
+            return (this.p1 + this.p2) / 2;
+        }
+
+       
+        // Nome
+        public void DefinirNome(string nome)
+        {
+            this.nome = nome;
+        }
+
+        public string GetNome()
+        {
+            return this.nome;
+        }
+
+        // Idade
+        public void DefinirIdade(int idade)
+        {
+            this.idade = idade;
+        }
+
+        public int GetIdade()
+        {
+            return this.idade;
+        }
+
+        // Matricula
+        public void DefinirMatricula(string matricula)
+        {
+            this.matricula = matricula;
+        }
+
+        public string GetMatricula()
+        {
+            return this.matricula;
+        }
+
+        // P1
+        public void DefinirP1(double p1)
+        {
+            this.p1 = p1;
+        }
+
+        public double GetP1()
+        {
+            return this.p1;
+        }
+
+        // P2
+        public void DefinirP2(double p2)
+        {
+            this.p2 = p2;
+        }
+
+        public double GetP2()
+        {
+            return this.p2;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno Igor, Vitor, Giovanni, Elder;
+
+            Igor = new Aluno();
+            Igor.DefinirNome("Igor");
+            Igor.DefinirIdade(20);
+            Igor.DefinirMatricula("00001");
+            Igor.DefinirP1(9.8);
+            Igor.DefinirP2(1.4);
+
+
+            Console.WriteLine("Nome:"  + Igor.GetNome() + "\n" + 
+                              "Idade:" + Igor.GetIdade() + "\n" + 
+                              "Matricula:" + Igor.GetMatricula() + "\n" + 
+                              "N1:" + Igor.GetP1() + "\n" +
+                              "N2:" + Igor.GetP2() + "\n" + 
+                              "Média:" + Igor.CalcularMedia()+ "\n");
+
+
+            Vitor = new Aluno();
+            Giovanni = new Aluno();
+            Elder = new Aluno();
+        }
+    }
+}
